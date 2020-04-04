1907c3c71f454cac8ce61e0557655cac619ec07d
Sho <developer@shota-ito.com>
2020-04-04 14:50:47 +0900

    Initial Commit

---
+++ SingSong/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SingSong
+//
+//  Created by Shota Ito on 2020/04/04.
+//  Copyright © 2020 Shota Ito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
